f205c3f2a5b938277790577b1ab7fb76256e5d83
Alex Gospodchikov <aleks-say@mail.ru>
2018-11-11 19:45:23 +0300

    Added methods to write to the databases.

--- db/db_write.py
+++ db/db_write.py
@@ -1,9 +1,18 @@
 from db.db import players, monsters
 
 
-def add_player(playerId):
-    pass
+def add_player(playerId, money=100):
+    # String set would make more sense, potentially, but I'm unsure if we can easily create it
+    players.put_item(Item={
+        "playerId": playerId,
+        "money": money,
+        "owned_monsters": list()
+    })
 
 
-def add_monster(playerId, monsterId):
-    pass
+def add_monster(playerId, monsterId, health=100, food=100, entertainment=100):
+    monsters.put_item(Item={"masterId": playerId,
+                            "monsterId": monsterId,
+                            "health": health,
+                            "food": food,
+                            "entertainment": entertainment})
